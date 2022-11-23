94aef413aa7be9d81d3005317d105de06cc0b53d
anilcanboga <anlcn.boga@hotmail.com>
2022-11-24 02:42:13 +0300

    folder structure updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,11 @@
-import './globals.css'
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+    <html lang="tr">
+      <body>
+        <header>Header</header>
+        <main>{children}</main>
+        <footer>Footer</footer>
+      </body>
     </html>
-  )
+  );
 }
